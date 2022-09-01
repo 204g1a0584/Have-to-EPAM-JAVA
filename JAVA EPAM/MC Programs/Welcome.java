cb64debdacf7882fe86f1b9ced6e434ca9389322
MC <79386814+204g1a0584@users.noreply.github.com>
2022-09-01 07:19:58 +0530

    Added files via upload
    added exam curriculum excel file
    added my programs folder which i have started practicing
    added Java Lab Codetantra Programs for Reference

---
+++ JAVA EPAM/MC Programs/Welcome.java
@@ -0,0 +1,14 @@
+class Welcome{
+	public static void main(String args[]){
+		System.out.printlln("Hi User\n Welcome to my first ever Java Program");
+/*println : after printing the statements,
+      the cursor moves to the next line  */
+//if \n is used them at that point cursor is moved to new line	  
+		System.out.println("Thank you");
+	}
+}
+
+//Note Downs
+/* 1.class keyword in smalls
+   2.String keyword starts with cap
+   */
